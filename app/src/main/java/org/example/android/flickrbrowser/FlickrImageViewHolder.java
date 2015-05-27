c2227ec2051ad4a6f98714e67a6c23461951d193
mynameismaxz <sirachat.sir@gmail.com>
2015-05-27 14:23:06 +0700

    Initial commit

---
+++ app/src/main/java/org/example/android/flickrbrowser/FlickrImageViewHolder.java
@@ -0,0 +1,21 @@
+package org.example.android.flickrbrowser;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by dev on 12/6/14.
+ * Updated to API 22 on 5/6/15
+ */
+public class FlickrImageViewHolder extends RecyclerView.ViewHolder {
+    protected ImageView thumbnail;
+    protected TextView title;
+
+    public FlickrImageViewHolder(View view) {
+        super(view);
+        this.thumbnail = (ImageView) view.findViewById(R.id.thumbnail);
+        this.title = (TextView) view.findViewById(R.id.title);
+    }
+}
